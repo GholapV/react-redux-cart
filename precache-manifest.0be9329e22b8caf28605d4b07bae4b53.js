f3f45f3f0169e8496a9b7b67af0c873331178f1b
GholapV <vishalgholap052@gmail.com>
2021-11-13 15:50:53 +0530

    Updates

---
+++ precache-manifest.0be9329e22b8caf28605d4b07bae4b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6039875da883f73b32fd19decfe6f39b",
+    "url": "/react-redux-cart/index.html"
+  },
+  {
+    "revision": "8a9fb4b62125fae2b53f",
+    "url": "/react-redux-cart/static/css/main.2bbfc7a0.chunk.css"
+  },
+  {
+    "revision": "28cc8c9e0e81627b725c",
+    "url": "/react-redux-cart/static/js/2.43013180.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/react-redux-cart/static/js/2.43013180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9fb4b62125fae2b53f",
+    "url": "/react-redux-cart/static/js/main.f18b044c.chunk.js"
+  },
+  {
+    "revision": "89df73748f71da54c236",
+    "url": "/react-redux-cart/static/js/runtime-main.73014951.js"
+  }
+]);
